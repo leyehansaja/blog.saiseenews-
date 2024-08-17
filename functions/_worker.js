3da6e1078893e01905504cbc7595c7e4280646e9
leyehansaja <96562607+leyehansaja@users.noreply.github.com>
2024-08-17 14:44:37 +0300

    Update _worker.js

--- functions/_worker.js
+++ functions/_worker.js
@@ -1,16 +1,15 @@
-export default {
-  async fetch(request, env) {
-    const url = new URL(request.url);
-    
-    // Jika path bukan root, arahkan ke Google Apps Script
-    if (url.pathname !== '/') {
-      const scriptUrl = 'https://script.google.com/macros/s/AKfycbwFh8x2TSx53_GvqMWWtIlpIb22MzMuXbtRC_xgEdfeuZ9XAI0k_pAWzLojU8RXl_nZUg/exec';
-      const newUrl = new URL(scriptUrl);
-      newUrl.searchParams.set('path', url.pathname);
-      return fetch(newUrl);
-    }
-    
-    // Jika root, lanjutkan ke aset statis (index.html)
-    return env.ASSETS.fetch(request);
+export function onRequest(context) {
+  const url = new URL(context.request.url);
+  console.log('Requested URL:', url.toString());
+  
+  if (url.pathname !== '/') {
+    const scriptUrl = 'https://script.google.com/macros/s/AKfycbwFh8x2TSx53_GvqMWWtIlpIb22MzMuXbtRC_xgEdfeuZ9XAI0k_pAWzLojU8RXl_nZUg/exec';
+    const newUrl = new URL(scriptUrl);
+    newUrl.searchParams.set('path', url.pathname);
+    console.log('Redirecting to:', newUrl.toString());
+    return fetch(newUrl);
   }
-};
+  
+  console.log('Serving index.html');
+  return context.env.ASSETS.fetch(context.request);
+}
