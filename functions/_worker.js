f985e680038ae4a60df9d0dbff0228bfea653797
leyehansaja <96562607+leyehansaja@users.noreply.github.com>
2024-08-18 02:28:38 +0300

    Update _worker.js

--- functions/_worker.js
+++ functions/_worker.js
@@ -1,15 +1,15 @@
-export function onRequest(context) {
+export async function onRequest(context) {
   const url = new URL(context.request.url);
   console.log('Requested URL:', url.toString());
   
-  if (url.pathname !== '/' && !url.pathname.startsWith('/assets/')) {
+  if (url.pathname === '/') {
+    console.log('Serving index.html');
+    return context.env.ASSETS.fetch(context.request);
+  } else {
     const scriptUrl = 'https://script.google.com/macros/s/AKfycbwFh8x2TSx53_GvqMWWtIlpIb22MzMuXbtRC_xgEdfeuZ9XAI0k_pAWzLojU8RXl_nZUg/exec';
     const newUrl = new URL(scriptUrl);
     newUrl.searchParams.set('path', url.pathname);
     console.log('Redirecting to:', newUrl.toString());
     return fetch(newUrl);
   }
-  
-  console.log('Serving index.html');
-  return context.env.ASSETS.fetch(context.request);
 }
