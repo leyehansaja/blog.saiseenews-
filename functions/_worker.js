28f4a0b1a27cb9eec9a075f7da250c1a0b6d0c7c
leyehansaja <96562607+leyehansaja@users.noreply.github.com>
2024-08-18 02:18:37 +0300

    Update _worker.js

--- functions/_worker.js
+++ functions/_worker.js
@@ -2,7 +2,7 @@ export function onRequest(context) {
   const url = new URL(context.request.url);
   console.log('Requested URL:', url.toString());
   
-  if (url.pathname !== '/') {
+  if (url.pathname !== '/' && !url.pathname.startsWith('/assets/')) {
     const scriptUrl = 'https://script.google.com/macros/s/AKfycbwFh8x2TSx53_GvqMWWtIlpIb22MzMuXbtRC_xgEdfeuZ9XAI0k_pAWzLojU8RXl_nZUg/exec';
     const newUrl = new URL(scriptUrl);
     newUrl.searchParams.set('path', url.pathname);
