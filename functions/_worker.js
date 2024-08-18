738637f4f47f4bf8194ef5ec2bff4ba7611f4dbd
leyehansaja <96562607+leyehansaja@users.noreply.github.com>
2024-08-18 07:38:53 +0300

    Update _worker.js

--- functions/_worker.js
+++ functions/_worker.js
@@ -6,7 +6,7 @@ export async function onRequest(context) {
     console.log('Serving index.html');
     return context.env.ASSETS.fetch(context.request);
   } else {
-    const scriptUrl = 'https://script.google.com/macros/s/AKfycbzalX7tNObq2fEdug7HQxeuy26sUiUtDHmWU1zvmWjYLhl_b_UgleSb-GH9vqyic4p5/exec';
+    const scriptUrl = 'https://script.google.com/macros/s/AKfycbyRbl41R4G9FYnItniE_wxn-Nqz_EzGPRJk8-_cJlJnTJ8nKkJuJjxdXEoA151ekl-9vQ/exec';
     const newUrl = new URL(scriptUrl);
     newUrl.searchParams.set('path', url.pathname);
     console.log('Redirecting to:', newUrl.toString());
