513e432ba0f1cb0da18c9240b7fc832d7c442f6d
leyehansaja <96562607+leyehansaja@users.noreply.github.com>
2024-08-21 03:02:20 +0300

    Update _worker.js

--- functions/_worker.js
+++ functions/_worker.js
@@ -1,19 +1,23 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
   console.log('Requested URL:', url.toString());
-  
+
   if (url.pathname === '/') {
     console.log('Serving index.html');
     return context.env.ASSETS.fetch(context.request);
   } else {
-    const scriptUrl = 'https://script.google.com/macros/s/AKfycbyRbl41R4G9FYnItniE_wxn-Nqz_EzGPRJk8-_cJlJnTJ8nKkJuJjxdXEoA151ekl-9vQ/exec';
+    const scriptUrl = 'https://script.google.com/macros/s/AKfycbz-_YfM4FuqA5eBQduKW7bujHCMF9sUXI_wrMSWBAj_KJI8K5ur_mC3zYS3l1PqJQHZvw/exec';
     const newUrl = new URL(scriptUrl);
     newUrl.searchParams.set('path', url.pathname);
     console.log('Redirecting to:', newUrl.toString());
-    const response = await fetch(newUrl);
+    const response = await fetch(newUrl, {
+      headers: {
+        'X-Frame-Options': 'ALLOWALL'
+      }
+    });
     const html = await response.text();
     return new Response(html, {
-      headers: { 'Content-Type': 'text/html' },
+      headers: { 'Content-Type': 'text/html' }
     });
   }
 }
